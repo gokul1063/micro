39540bf0b05a5f004b8a64dfd6fdbaf3b073f189
Gokul <gokulrajeshkumar1063@gmail.com>
2025-09-12 02:29:01 +0530

    Horizontal scroll is working!!

--- micro.c
+++ micro.c
@@ -27,6 +27,7 @@ struct editorConfig{
   int cx , cy ;
   int screenrows;
   int screencols;
+  int coloff;
   int numrows;
   int rowoff;
   erow *row;
@@ -245,9 +246,7 @@ void editorMoveCursor(int key){
       }
       break;
     case ARROW_RIGHT:
-      if (E.cx != E.screencols - 1) {
         E.cx++;
-      }
       break;
     case ARROW_UP:
       if (E.cy != 0) {
@@ -306,6 +305,14 @@ void editorScroll(){
   if (E.cy >= E.rowoff + E.screenrows){
     E.rowoff = E.cy - E.screenrows + 1;
   }
+
+  if (E.cx < E.coloff) {
+    E.coloff = E.cx;
+
+  }
+  if (E.cx >= E.coloff + E.screencols){
+    E.coloff = E.cx - E.screencols;
+  }
 }
 
 void editorDrawRows(struct abuf *ab){
@@ -330,9 +337,10 @@ void editorDrawRows(struct abuf *ab){
         abAppend(ab , "~",1);
       }
     } else {
-      int len = E.row[filerow].size;
+      int len = E.row[filerow].size - E.coloff;
+      if (len < 0) len = 0;
       if (len > E.screencols) len = E.screencols;
-      abAppend(ab , E.row[filerow].chars , len);
+      abAppend(ab , &E.row[filerow].chars[E.coloff] , len);
     }
     
     abAppend(ab , "\x1b[K" , 3);
@@ -354,7 +362,8 @@ void editorRefreshScreen(){
   editorDrawRows(&ab);
 
   char buf[32];
-  snprintf(buf , sizeof(buf) , "\x1b[%d;%dH" , (E.cy - E.rowoff) + 1 , E.cx + 1);
+  snprintf(buf , sizeof(buf) , "\x1b[%d;%dH" , (E.cy - E.rowoff) + 1,
+                                               (E.cx - E.coloff) + 1);
   abAppend(&ab , buf , strlen(buf));
 
   
@@ -368,6 +377,7 @@ void editorRefreshScreen(){
 void initEditor() {
   E.cx = 0;
   E.cy = 0;
+  E.coloff = 0;
   E.numrows = 0;
   E.rowoff = 0;
   E.row = NULL;
