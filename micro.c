601ce49aea15d1af863819fe238a6136d1a09f83
Gokul <gokulrajeshkumar1063@gmail.com>
2025-09-12 01:49:35 +0530

    In this version the code actually gets the file name form the user and prints all the possible line still scroll is not activated but ill complete it by tomorrow atleast
    And today i learned about micros and some of them are
    _GUN_SOURCE
    _DEFAULT_SOURCE
    _BSD_SOURCE
    
    the thse are the micros which tells how the modules
    are supposed to be installed in the code like getline()
    and so on

--- micro.c
+++ micro.c
@@ -1,9 +1,14 @@
 /** includes **/
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <stdio.h>
 #include <errno.h>
 
@@ -13,10 +18,17 @@
 #define MICRO_VERSION "0.0.1"
 
 /** data **/
+typedef struct erow {
+  int size;
+  char *chars;
+} erow;
+
 struct editorConfig{
   int cx , cy ;
   int screenrows;
   int screencols;
+  int numrows;
+  erow *row;
   struct termios orgin_termios;
 };
 
@@ -27,7 +39,6 @@ struct abuf{
   char* buffer;
   int len;
 };
-
 
 enum editorKey{
   ARROW_lEFT = 1000 ,
@@ -191,7 +202,37 @@ int getWindowSize(int *row , int *col){
 
 }
 
+/*** row operations ***/
+void editorAppendRow(char *s , size_t len){
+  E.row = realloc(E.row , sizeof(erow) * (E.numrows + 1));
+ 
+  int at = E.numrows;
+  E.row[at].size = len;
+  E.row[at].chars = malloc(len + 1);
+  memcpy(E.row[at].chars , s , len);
+  E.row[at].chars[len] = '\0';
+  E.numrows++;
 
+}
+
+/*** file i/0 ***/
+void editorOpen(char *filename) {
+  FILE *fp = fopen(filename , "r");
+  if (!fp) die("fopen");
+
+  char *line = NULL;
+  size_t linecap = 0;
+  ssize_t linelen; 
+  while((linelen = getline(&line , &linecap , fp)) != -1){
+    while(linelen > 0 && (line[linelen - 1] == '\n'||
+                          line[linelen - 1] == '\r'))
+      linelen--;
+    editorAppendRow(line , linelen);
+  }
+  free(line);
+  fclose(fp);
+  
+}
 
 
 /*** input ***/
@@ -251,21 +292,27 @@ void editorProcessKey(){
 void editorDrawRows(struct abuf *ab){
   int y;
   for (y = 0 ; y < E.screenrows ; y++){
-    if (y == E.screenrows /3){
-      char welcome[80];
-      int welcomelen = snprintf(welcome , sizeof(welcome),
-          "miro editor -- version %s" , MICRO_VERSION);
-      if (welcomelen > E.screencols) welcomelen  = E.screencols;
-      int padding = (E.screencols - welcomelen ) / 2;
-      if (padding){
-        abAppend(ab , "~" , 1);
-        padding -- ;
-      }
-      while (padding--) abAppend(ab , " " , 1);
-      abAppend(ab , welcome , welcomelen);
+    if (y >= E.numrows) {
+      if (E.numrows == 0 && y == E.screenrows /3 ){
+        char welcome[80];
+        int welcomelen = snprintf(welcome , sizeof(welcome),
+            "miro editor -- version %s" , MICRO_VERSION);
+        if (welcomelen > E.screencols) welcomelen  = E.screencols;
+        int padding = (E.screencols - welcomelen ) / 2;
+        if (padding){
+          abAppend(ab , "~" , 1);
+          padding -- ;
+        }
+        while (padding--) abAppend(ab , " " , 1);
+        abAppend(ab , welcome , welcomelen);
 
+      } else {
+        abAppend(ab , "~",1);
+      }
     } else {
-      abAppend(ab , "~",1);
+      int len = E.row[y].size;
+      if (len > E.screencols) len = E.screencols;
+      abAppend(ab , E.row[y].chars , len);
     }
     
     abAppend(ab , "\x1b[K" , 3);
@@ -273,7 +320,6 @@ void editorDrawRows(struct abuf *ab){
       abAppend(ab,"\r\n", 2);
     }
   }
-
 
 }
 
@@ -300,13 +346,18 @@ void editorRefreshScreen(){
 void initEditor() {
   E.cx = 0;
   E.cy = 0;
+  E.numrows = 0;
+  E.row = NULL;
   if (getWindowSize(&E.screenrows , &E.screencols ) == -1 )
     die("getWindowSize");
 }
 
-int main() {
+int main(int argc , char *argv[]) {
   enableRawMode();
   initEditor();
+  if (argc >= 2){
+    editorOpen(argv[1]);
+  }
   while(1){
     editorRefreshScreen();
     editorProcessKey();
