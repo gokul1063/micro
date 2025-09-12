bc94217a9a30a381020099be6bd0f367e9002d68
Gokul <gokulrajeshkumar1063@gmail.com>
2025-09-12 18:33:15 +0530

    On this we have added the code in editorMoveCursor() to make sure that if we move press right or lest arrow at the end of the line it move according to it

--- micro.c
+++ micro.c
@@ -239,14 +239,23 @@ void editorOpen(char *filename) {
 
 /*** input ***/
 void editorMoveCursor(int key){
+  erow *row = (E.cy >= E.numrows ) ? NULL : &E.row[E.cy];
   switch (key){
     case ARROW_LEFT:
       if (E.cx != 0) {
         E.cx--;
+      } else if (E.cy > 0) {
+        E.cy --;
+        E.cx = E.row[E.cy].size;
       }
       break;
     case ARROW_RIGHT:
+      if(row && E.cx < row->size){
         E.cx++;
+      } else if (E.cy < E.screenrows){
+        E.cy ++;
+        E.cx = 0;
+      }
       break;
     case ARROW_UP:
       if (E.cy != 0) {
@@ -258,6 +267,12 @@ void editorMoveCursor(int key){
         E.cy++;
       }
       break;;
+  }
+  
+  row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  int rowlen = row ? row->size : 0;
+  if (E.cx > rowlen){
+    E.cx = rowlen;
   }
 }
 
