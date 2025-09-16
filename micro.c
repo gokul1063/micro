aee307fb4efb221dc831fcc94f2ecd6152681a2e
Gokul <gokulrajeshkumar1063@gmail.com>
2025-09-17 01:55:54 +0530

    fixed the coloring of number after tabs all by myself

--- micro.c
+++ micro.c
@@ -233,11 +233,11 @@ int getWindowSize(int *row , int *col){
 /*** syntax highlighting ***/
 
 void editorUpdateSyntax(erow *row){
-  row->hl = realloc(row->hl , row->size);
-  memset(row->hl , HL_NORMAL , row->size);
+  row->hl = realloc(row->hl , row->rsize);
+  memset(row->hl , HL_NORMAL , row->rsize);
 
   int i;
-  for (i = 0 ; i < row->size ; i++){
+  for (i = 0 ; i < row->rsize ; i++){
     if (isdigit(row->render[i])){
       row->hl[i] = HL_NUMBER;
     }
