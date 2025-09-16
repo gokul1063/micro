05348c77288b7a00d78ebbf01b90fb4a4fb3b9a6
Gokul <gokulrajeshkumar1063@gmail.com>
2025-09-17 02:33:42 +0530

    coloring added for '.' in decimal Eg. 3.2

--- micro.c
+++ micro.c
@@ -251,7 +251,7 @@ void editorUpdateSyntax(erow *row){
 
     unsigned char prev_hl = (i > 0) ? row->hl[i-1] : HL_NORMAL;
 
-    if (isdigit(c) && (prev_sep || prev_hl == HL_NUMBER) ){
+    if ( ( isdigit(c) && (prev_sep || prev_hl == HL_NUMBER) ) || (c == '.' && prev_hl == HL_NUMBER ) ){
       row->hl[i] = HL_NUMBER;
       i++;
       prev_sep = 0;
