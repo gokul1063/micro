31d56f9e7429496631fedc850fa636d6dee2ba4d
Gokul <gokulrajeshkumar1063@gmail.com>
2025-09-17 15:24:20 +0530

    added color for keywords (2 different colors)

--- micro.c
+++ micro.c
@@ -79,6 +79,8 @@ enum editorKey{
 enum editorHighlight{
   HL_NORMAL = 0,
   HL_COMMENT,
+  HL_kEYWORD1,
+  HL_KEYWORD2,
   HL_STRING,
   HL_NUMBER,
   HL_MATCH
@@ -88,6 +90,7 @@ enum editorHighlight{
 struct editorSyntax {
   char *filetype;
   char **filematch;
+  char **keywords;
   char *singleline_comments_start;
   int flag;
 };
@@ -97,11 +100,18 @@ struct editorSyntax {
 /*** filetypes ***/
 
 char *C_HL_extensions[] = {".c" , ".h" , ".cpp" , NULL};
+char *C_HL_keywords[] = {
+  "switch", "if", "while", "for", "break", "continue", "return", "else",
+  "struct", "union", "typedef", "static", "enum", "class", "case",
+  "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
+  "void|", NULL
+};
 
 struct editorSyntax HLDB[] = {
   {
     "c",
     C_HL_extensions,
+    C_HL_keywords,
     "//",
     HL_HILIGHT_NUMBERS | HL_HILIGHT_STRING
   },
@@ -281,6 +291,8 @@ void editorUpdateSyntax(erow *row){
   int prev_sep = 1;
   int in_string = 0;
 
+  char **keywords = E.syntax->keywords;
+
   char *scs = E.syntax->singleline_comments_start;
   int scs_len = scs ? strlen(scs) : 0;
 
@@ -329,6 +341,28 @@ void editorUpdateSyntax(erow *row){
       }
     }
 
+    if (prev_sep){
+      int j;
+      for (j = 0 ; keywords[j] ; j++){
+        int klen = strlen(keywords[j]);
+        int kw2 = keywords[j][klen-1] == '|';
+        if (kw2) klen--;
+
+
+        if (!strncmp(&row->render[i] , keywords[j] , klen) && 
+            (is_separator(row->render[i+klen]) ) ){
+          memset(&row->hl[i],kw2 ? HL_KEYWORD2 : HL_kEYWORD1, klen);
+          i += klen;
+          break;
+        }
+      }
+
+      if (keywords[j] != NULL){
+        prev_sep = 0;
+        continue;
+      }
+    }
+
     prev_sep = is_separator(c);
     i++;
   }
@@ -339,6 +373,8 @@ void editorUpdateSyntax(erow *row){
 int editorSyntaxToColor(int hl){
   switch(hl){
     case HL_NUMBER : return 31;
+    case HL_kEYWORD1 : return 33;
+    case HL_KEYWORD2 : return 32;                       
     case HL_COMMENT : return 36;
     case HL_STRING : return 35;
     case HL_MATCH : return 34;
